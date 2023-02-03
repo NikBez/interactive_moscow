fb6b206bf12dd84bf58c9247fe9cc0451ef20b76
Kit <nikitabit@yandex.ru>
2023-02-03 19:26:28 +0700

    Add model for Images

--- places/admin.py
+++ places/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from places.models import Place
+from places.models import Place, Image
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('title', 'lng', 'lat', 'short_description')
-    fields = ('title', ('lng', 'lat'), 'short_description', 'long_description')
+    fields = ('title', ('lng', 'lat'), 'short_description', 'long_description', 'images')
+    filter_horizontal = ['images']
 
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
--- places/models.py
+++ places/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField("Название", max_length=100)
+    images = models.ManyToManyField("Image", verbose_name="Изображение", blank=True, related_name="places")
     short_description = models.CharField("Краткое описание", max_length=300, blank=True)
     long_description = models.TextField("Полное описание", blank=True)
     lng = models.FloatField('Долгота')
@@ -9,4 +10,10 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField("Картинка", upload_to="places/")
+
+
 
