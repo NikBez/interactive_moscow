75186601a22024591eae4994539ed38b29aac068
Kit <nikitabit@yandex.ru>
2023-02-04 20:54:14 +0700

    Add django-tinymce HTML field in admin page

--- places/models.py
+++ places/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from tinymce import models as tinymce_models
+
 
 class Place(models.Model):
     title = models.CharField("Название", max_length=100)
@@ -8,7 +10,7 @@ class Place(models.Model):
                                     related_name="places",
                                     through='ImagesPlace')
     short_description = models.CharField("Краткое описание", max_length=300, blank=True)
-    long_description = models.TextField("Полное описание", blank=True)
+    long_description = tinymce_models.HTMLField("Полное описание", blank=True)
     lng = models.FloatField('Долгота')
     lat = models.FloatField('Широта')
 
