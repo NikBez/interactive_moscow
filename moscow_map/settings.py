85052466d049c2076a827ac1d200b2759654c4e3
Kit <nikitabit@yandex.ru>
2023-02-28 12:14:04 +0700

    Add static file root for deploy

--- moscow_map/settings.py
+++ moscow_map/settings.py
@@ -125,8 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = '/home/username/projects/site/assets/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
