4d05dccc8ee6d1d3235b8034b57c137fcbdd4994
Kit <nikitabit@yandex.ru>
2023-02-28 21:24:38 +0700

    Prepare for deploy

--- moscow_map/settings.py
+++ moscow_map/settings.py
@@ -131,8 +131,8 @@ if DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
         ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# else:
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
