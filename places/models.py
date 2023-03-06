253591c9137c1bda45b0cda2bfadd76f4f50f046
Kit <nikitabit@yandex.ru>
2023-03-06 10:45:59 +0700

    Remove static collected folder

--- places/models.py
+++ places/models.py
@@ -6,13 +6,13 @@ class Place(models.Model):
 
     title = models.CharField('Название', max_length=100)
     short_description = models.TextField(
-                                        'Краткое описание',
-                                         blank=True,
-                                         )
+        'Краткое описание',
+        blank=True,
+    )
     long_description = tinymce_models.HTMLField(
-                                                'Полное описание',
-                                                blank=True,
-                                                )
+        'Полное описание',
+        blank=True,
+    )
     lng = models.FloatField('Долгота')
     lat = models.FloatField('Широта')
 
@@ -22,7 +22,12 @@ class Place(models.Model):
 
 class Image(models.Model):
     image = models.ImageField('Картинка', upload_to='places/')
-    place = models.ForeignKey(Place, on_delete=models.CASCADE, null=True, related_name='images')
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='images'
+    )
 
     class Meta:
         ordering = ('image',)
--- places/views.py
+++ places/views.py
@@ -6,44 +6,46 @@ from places.models import Place
 
 
 def view_main_page(request):
-
     places = Place.objects.all()
-
     features = []
     for place in places:
-        features.append({
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [place.lng, place.lat]
-            },
-            'properties': {
-                'title': place.title,
-                'placeId': place.pk,
-                'detailsUrl': reverse('places', kwargs={'id': place.id}),
+        features.append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.lng, place.lat]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.pk,
+                    "detailsUrl": reverse("places", kwargs={"id": place.id}),
+                },
             }
-        })
+        )
 
     context = {
-        'geo': {
-            'type': 'FeatureCollection',
-            'features': features,
+        "geo": {
+            "type": "FeatureCollection",
+            "features": features,
         },
     }
-    return render(request, 'places/index.html', context=context)
+    return render(request, "places/index.html", context=context)
 
 
 def place_page_view(request, id):
-
     place = get_object_or_404(Place, id=id)
     place_detales = {
-        'title': place.title,
-        'imgs': [img.image.url for img in place.images.all()],
-        'description_short': place.short_description,
-        'description_long': place.long_description,
-        'coordinates': {
-            'lng': place.lng,
-            'lat': place.lat,
-            }
-        }
-    return JsonResponse(place_detales, json_dumps_params={'ensure_ascii': False})
+        "title": place.title,
+        "imgs": [img.image.url for img in place.images.all()],
+        "description_short": place.short_description,
+        "description_long": place.long_description,
+        "coordinates": {
+            "lng": place.lng,
+            "lat": place.lat,
+        },
+    }
+    return JsonResponse(
+        place_detales,
+        json_dumps_params={"ensure_ascii": False}
+    )
